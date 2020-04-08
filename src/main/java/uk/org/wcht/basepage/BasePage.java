db2a9c79d2d6b4a97464237d8abd5ed455693fa9
PradipDhanani <pradip.dhanani@gmail.com>
2020-04-08 11:42:10 +0100

    watford-community

---
+++ src/main/java/uk/org/wcht/basepage/BasePage.java
@@ -0,0 +1,12 @@
+package uk.org.wcht.basepage;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Pradip
+ */
+public class BasePage {
+    //define web driver
+    public static WebDriver driver;
+
+}
